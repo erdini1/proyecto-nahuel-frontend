766be9e8b1f1d8b2810aa58e7687e4f5934246be
Dylan Erdini <erdini.dylan@gmail.com>
2024-08-04 11:53:49 -0300

    Agregue los totales en el registro de caja

--- src/components/component/cashRegisterAdmin/CashRegisterAdminTable.jsx
+++ src/components/component/cashRegisterAdmin/CashRegisterAdminTable.jsx
@@ -5,11 +5,29 @@ import { ChevronDownIcon } from '@radix-ui/react-icons';
 export default function CashRegisterAdminTable({ cashRegisters, cashMovements, cancellations }) {
 
 	const cashMovementsFiltered = (cashRegisterId) => {
-		return cashMovements.filter((cashMovement) => cashMovement.cashRegisterId === cashRegisterId);
+		return cashMovements.filter((cashMovement) => cashMovement?.cashRegisterId === cashRegisterId);
 	}
 
 	const cancellationsFiltered = (cashRegisterId) => {
-		return cancellations.filter((cancellation) => cancellation.cashRegisterId === cashRegisterId);
+		return cancellations.filter((cancellation) => cancellation?.cashRegisterId === cashRegisterId);
+	}
+
+	const allPaymentMethodsTotals = () => {
+		return cashRegisters.reduce((acc, cashRegister) => {
+			return acc + cashRegister.totalPaymentMethods;
+		}, 0)
+	}
+
+	const allDiffTotals = () => {
+		return cashRegisters.reduce((acc, cashRegister) => {
+			return acc + cashRegister.totalDiff;
+		}, 0)
+	}
+
+	const cancellationsLength = () => {
+		return cashRegisters.reduce((acc, cashRegister) => {
+			return acc + cancellationsFiltered(cashRegister.id).length;
+		}, 0)
 	}
 
 	return (
@@ -17,15 +35,14 @@ export default function CashRegisterAdminTable({ cashRegisters, cashMovements, c
 			<Table className="min-w-full divide-y divide-gray-200">
 				<TableHeader className="bg-gray-100 text-left">
 					<TableRow>
-						<TableHead className="px-3 text-center">
-							<ChevronDownIcon className={`h-4 w-4 text-transparent`} />
+						<TableHead className="px-4">
 						</TableHead>
 						<TableHead className="w-2/12 py-3">Cajero</TableHead>
 						<TableHead className="w-2/12 py-3">Fecha</TableHead>
 						<TableHead className="w-1/12 py-3">Caja</TableHead>
 						<TableHead className="w-2/12 py-3">Total Medios de pago</TableHead>
-						<TableHead className="w-2/12 py-3">Total Retiros Tarjetas</TableHead>
-						<TableHead className="w-2/12 py-3">Total Retios Efectivo</TableHead>
+						<TableHead className="w-2/12 py-3">Total Diferencias</TableHead>
+						<TableHead className="w-2/12 py-3">Total Anulaciones</TableHead>
 						<TableHead className="w-1/12 py-3">Estado</TableHead>
 					</TableRow>
 				</TableHeader>
@@ -44,61 +61,20 @@ export default function CashRegisterAdminTable({ cashRegisters, cashMovements, c
 							/>
 						))
 					)}
+					{cashRegisters.length !== 0 && (
+						<TableRow className="bg-yellow-100 hover:bg-yellow-50">
+							<TableCell className="py-4"></TableCell>
+							<TableCell className="w-2/12 py-4"></TableCell>
+							<TableCell className="w-2/12 py-4"></TableCell>
+							<TableCell className="w-1/12 py-4 text-sm font-semibold uppercase">{/* Totales: */}</TableCell>
+							<TableCell className="w-2/12 py-4 font-semibold">$ {allPaymentMethodsTotals()}</TableCell>
+							<TableCell className={`w-2/12 py-4 font-semibold ${allDiffTotals() >= 0 ? "text-green-500" : "text-red-500"}`}>$ {allDiffTotals()}</TableCell>
+							<TableCell className="w-2/12 py-4 font-semibold">{cancellationsLength()}</TableCell>
+							<TableCell className="w-2/12 py-4 font-bold"></TableCell>
+						</TableRow>
+					)}
 				</TableBody>
 			</Table>
 		</div >
 	);
 }
-
-// EL de abajo anda
-// import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table";
-// import CashRegisterRow from '@/components/component/cashRegisterAdmin/CashRegisterRow';
-// import { ChevronDownIcon } from '@radix-ui/react-icons';
-
-// export default function CashRegisterAdminTable({ cashRegisters, cashMovements, cancellations }) {
-
-// 	const cashMovementsFiltered = (cashRegisterId) => {
-// 		return cashMovements.filter((cashMovement) => cashMovement.CashRegister.id === cashRegisterId);
-// 	}
-
-// 	const cancellationsFiltered = (cashRegisterId) => {
-// 		return cancellations.filter((cancellation) => cancellation.CashRegister.id === cashRegisterId);
-// 	}
-
-// 	return (
-// 		<div className="border shadow-sm rounded-lg  overflow-x-auto">
-// 			<Table className="min-w-full divide-y divide-gray-200">
-// 				<TableHeader className="bg-gray-100 text-left">
-// 					<TableRow>
-// 						<TableHead className="px-3 text-center">
-// 							<ChevronDownIcon className={`h-4 w-4 text-transparent`} />
-// 						</TableHead>
-// 						<TableHead className="w-2/12 py-3">Cajero</TableHead>
-// 						<TableHead className="w-2/12 py-3">Fecha</TableHead>
-// 						<TableHead className="w-1/12 py-3">Caja</TableHead>
-// 						<TableHead className="w-2/12 py-3">Total Medios de pago</TableHead>
-// 						<TableHead className="w-2/12 py-3">Total Retiros Tarjetas</TableHead>
-// 						<TableHead className="w-2/12 py-3">Total Retios Efectivo</TableHead>
-// 						<TableHead className="w-1/12 py-3">Estado</TableHead>
-// 					</TableRow>
-// 				</TableHeader>
-// 				<TableBody className="bg-white divide-y divide-gray-200">
-// 					{cashRegisters.length === 0 ? (
-// 						<TableRow>
-// 							<TableCell colSpan="7" className="px-6 py-4 whitespace-nowrap text-center">No hay registros de caja</TableCell>
-// 						</TableRow>
-// 					) : (
-// 						cashRegisters.map((cashRegister) => (
-// 							<CashRegisterRow
-// 								key={cashRegister.id}
-// 								cashRegister={cashRegister}
-// 								cashMovementsFiltered={cashMovementsFiltered}
-// 								cancellationsFiltered={cancellationsFiltered}
-// 							/>
-// 						))
-// 					)}
-// 				</TableBody>
-// 			</Table>
-// 		</div >
-// 	);
-// }
--- src/components/component/cashRegisterAdmin/CashRegisterRow.jsx
+++ src/components/component/cashRegisterAdmin/CashRegisterRow.jsx
@@ -24,8 +24,8 @@ const CashRegisterRow = ({ cashRegister, cashMovementsFiltered, cancellationsFil
 				<TableCell className="w-2/12 py-4">{cashRegister.date}</TableCell>
 				<TableCell className="w-1/12 py-4">{cashRegister.cashBoxNumber}</TableCell>
 				<TableCell className="w-2/12 py-4">$ {cashRegister.totalPaymentMethods}</TableCell>
-				<TableCell className="w-2/12 py-4">$ {cashRegister.cashWithdrawal}</TableCell>
-				<TableCell className="w-2/12 py-4">$ {cashRegister.cardWithdrawal}</TableCell>
+				<TableCell className={`w-2/12 py-4 ${cashRegister.totalDiff >= 0 ? "text-green-500" : "text-red-500"}`}>$ {cashRegister.totalDiff}</TableCell>
+				<TableCell className="w-2/12 py-4">{cancellationsFiltered(cashRegister?.id).length}</TableCell>
 				<TableCell className="w-1/12 py-4">
 					<Badge className={`rounded-2xl p-1 text-xs ${cashRegister.isClosed ? 'bg-green-500 hover:bg-green-600' : 'bg-yellow-500 hover:bg-yellow-600'}`}>
 						{cashRegister.isClosed ? 'Cerrada' : 'Abierta'}
@@ -62,92 +62,3 @@ const CashRegisterRow = ({ cashRegister, cashMovementsFiltered, cancellationsFil
 };
 
 export default CashRegisterRow;
-
-// EL de abajo anda
-// import { useEffect, useState } from 'react';
-// import { TableRow, TableCell } from "@/components/ui/table";
-// import { ChevronDownIcon } from '@radix-ui/react-icons';
-// import { Collapsible, CollapsibleContent } from "@/components/ui/collapsible"
-// import { Badge } from "@/components/ui/badge"
-// import PaymentMethodsTable from '@/components/component/cashRegisterAdmin/PaymentMethodsTable';
-// import CashMovementsTable from '@/components/component/cashRegisterAdmin/CashMovementsTable';
-// import CancellationsTable from '@/components/component/cashRegisterAdmin/CancellationsTable';
-// import { format } from 'date-fns';
-// import { toZonedTime } from 'date-fns-tz'
-
-// const CashRegisterRow = ({ cashRegister, cashMovementsFiltered, cancellationsFiltered }) => {
-// 	const [isExpanded, setIsExpanded] = useState(false);
-// 	const [totalPaymentMethods, setTotalPaymentMethods] = useState(0);
-// 	const [totalWithdrawalCash, setTotalWithdrawalCash] = useState(0);
-// 	const [totalWithdrawalCards, setTotalWithdrawalCards] = useState(0);
-
-// 	const handleRowClick = () => {
-// 		setIsExpanded(!isExpanded);
-// 	};
-
-// 	useEffect(() => {
-// 		if (cashRegister) {
-// 			const totalPaymentMethods = Object.keys(cashRegister).filter(key => key.includes('salesWith')).reduce((sum, key) => sum + parseFloat(cashRegister[key] || 0), 0);
-// 			setTotalPaymentMethods(totalPaymentMethods);
-
-// 			const totalWithdrawalCash = calculateWithdrawal(cashMovementsFiltered(cashRegister.id), cancellationsFiltered(cashRegister.id), 'EFECTIVO');
-// 			setTotalWithdrawalCash(totalWithdrawalCash);
-
-// 			const totalWithdrawalCards = calculateWithdrawal([], cancellationsFiltered(cashRegister.id), 'CLOVER');
-// 			setTotalWithdrawalCards(totalWithdrawalCards);
-// 		}
-// 	}, [cashRegister, cashMovementsFiltered, cancellationsFiltered]);
-
-// 	const calculateWithdrawal = (cashMovements, cancellations, method) => {
-// 		return cashMovements.reduce((sum, movement) => sum + parseFloat(movement.amount || 0), 0) +
-// 			(cancellations.filter(cancellation => cancellation.method.includes(method)).reduce((sum, cancellation) => sum + parseFloat(cancellation.amount || 0), 0));
-// 	};
-
-// 	return (
-// 		<>
-// 			<TableRow onClick={handleRowClick} className={`whitespace-nowrap cursor-pointer ${isExpanded ? 'bg-gray-200 hover:bg-gray-100' : 'hover:bg-gray-50'}`}>
-// 				<TableCell className="py-4 cursor-pointer">
-// 					<ChevronDownIcon className={`h-4 w-4 ${isExpanded ? 'transform rotate-180' : ''}`} />
-// 				</TableCell>
-// 				<TableCell className="w-2/12 py-4 capitalize">{cashRegister.User.firstName} {cashRegister.User.lastName}</TableCell>
-// 				<TableCell className="w-2/12 py-4">{format(toZonedTime(cashRegister.date, 'America/Argentina/Ushuaia'), 'dd/MM')}</TableCell>
-// 				<TableCell className="w-1/12 py-4">{cashRegister.CashBox.description}</TableCell>
-// 				<TableCell className="w-2/12 py-4">$ {totalPaymentMethods}</TableCell>
-// 				<TableCell className="w-2/12 py-4">$ {totalWithdrawalCards}</TableCell>
-// 				<TableCell className="w-2/12 py-4">$ {totalWithdrawalCash}</TableCell>
-// 				<TableCell className="w-1/12 py-4">
-// 					<Badge className={`rounded-2xl p-1 text-xs ${cashRegister.isClosed ? 'bg-green-500 hover:bg-green-600' : 'bg-yellow-500 hover:bg-yellow-600'}`}>
-// 						{cashRegister.isClosed ? 'Cerrada' : 'Abierta'}
-// 					</Badge>
-// 				</TableCell>
-// 			</TableRow>
-// 			{isExpanded && (
-// 				<TableRow>
-// 					<TableCell colSpan="8" className="px-6 py-4 whitespace-nowrap bg-gray-200/40">
-// 						<Collapsible open>
-// 							<CollapsibleContent className='flex flex-col gap-4'>
-// 								<PaymentMethodsTable
-// 									cashRegister={cashRegister}
-// 									cashMovements={cashMovementsFiltered(cashRegister.id)}
-// 									cancellations={cancellationsFiltered(cashRegister.id)}
-// 								/>
-// 								<div className='flex gap-2'>
-// 									<CashMovementsTable cashMovements={cashMovementsFiltered(cashRegister.id)} />
-// 									<CancellationsTable cancellations={cancellationsFiltered(cashRegister.id)} />
-// 								</div>
-// 								<div className="flex gap-6 border rounded-lg bg-white shadow p-4 text-xs">
-// 									<div className="flex items-center gap-1">
-// 										<span className="font-semibold text-gray-700 uppercase">Observaciones:</span>
-// 										<p>{cashRegister?.observations ? cashRegister.observations : 'Sin observaciones'}</p>
-// 									</div>
-// 								</div>
-// 							</CollapsibleContent>
-// 						</Collapsible>
-// 					</TableCell>
-// 				</TableRow>
-// 			)}
-// 		</>
-// 	);
-// };
-
-// export default CashRegisterRow;
